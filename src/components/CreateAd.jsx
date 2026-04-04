1264317f16f900eaadf4b75c53f0e2f27f811528
Sama Thaher <samathaher047@gmail.com>
2026-04-04 12:17:06 +0300

    style: refine CreateAd layout and spacing

--- src/components/CreateAd.jsx
+++ src/components/CreateAd.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios"; 
 import Navbar from "./navbar";
-// استيراد الأيقونات من مكتبة react-icons
 import { 
   FaCameraRetro, 
   FaLightbulb, 
